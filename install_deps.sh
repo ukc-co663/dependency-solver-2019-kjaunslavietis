62c5dbbcbb1ec32fd8c91e9a2cd0479be268f789
Kārlis Jaunslavietis <kjaunslavietis@gmail.com>
2019-02-26 22:51:04 +0000

    Update install_deps

--- install_deps.sh
+++ install_deps.sh
@@ -2,4 +2,5 @@
 rm -rf "lib/*"
 mkdir -p "lib"
 wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
+wget -O lib/logicng-1.4.1.jar https://search.maven.org/remotecontent?filepath=org/logicng/logicng/1.4.1/logicng-1.4.1.jar
 
